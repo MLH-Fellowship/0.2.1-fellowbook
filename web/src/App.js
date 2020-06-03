def235be1706f2892ea8364cdbc6a61c425e9993
Kenneth Aladi <kennethmichael11@gmail.com>
2020-06-03 12:57:15 +0100

    created the Header with basic styling

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
+import Header from "./Components/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <Header />
+      </header>
     </div>
   );
 }
--- web/src/Components/Header.js
+++ web/src/Components/Header.js
@@ -1,10 +1,24 @@
 import React from "react";
+import image from "../img/mlh.png";
 
 const Header = () => {
   return (
-    <div>
-      <img src="../img/mlh.png" alt="MLH logo" />
-      <h1>FellowBook</h1>
+    <div
+      style={{
+        display: "flex",
+        backgroundColor: "grey",
+        width: "100%",
+        color: "white",
+        justifyContent: "space-between",
+        padding: "0 20px",
+      }}
+    >
+      <img
+        src={image}
+        alt="MLH logo"
+        style={{ width: "150px", height: "150px" }}
+      />
+      <h1 style={{ paddingTop: "40px" }}>FellowBook</h1>
     </div>
   );
 };
