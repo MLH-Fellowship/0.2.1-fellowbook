3ebabdd3dc5b001601d28dc917ecba1cf3cf5581
Kenneth Aladi <kennethmichael11@gmail.com>
2020-06-04 00:53:49 +0100

    Changed the top link to a github icon

--- web/src/App.js
+++ web/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./App.css";
 import Header from "./Components/Header";
 import Search from "./Components/Search";
+import Card from "./Components/Card";
+import "font-awesome/css/font-awesome.min.css";
 
 function App() {
   return (
@@ -12,6 +14,64 @@ function App() {
           <Search />
         </div>
       </header>
+      <main className="container">
+        <Card
+          title="Toledo, Spain"
+          subtitle="Also know as The Imperial City."
+          tag="Spain"
+          iconName="fa fa-github"
+          bgPhoto="https://picsum.photos/740/420/?random"
+        />
+        <Card
+          title="Toledo, Spain"
+          subtitle="Also know as The Imperial City."
+          tag="Spain"
+          iconName="fas fa-ellipsis-v"
+          bgPhoto="https://picsum.photos/740/420/?random"
+        />
+        <Card
+          title="Toledo, Spain"
+          subtitle="Also know as The Imperial City."
+          tag="Spain"
+          iconName="fas fa-ellipsis-v"
+          bgPhoto="https://picsum.photos/740/420/?random"
+        />
+        <Card
+          title="Toledo, Spain"
+          subtitle="Also know as The Imperial City."
+          tag="Spain"
+          iconName="fas fa-ellipsis-v"
+          bgPhoto="https://picsum.photos/740/420/?random"
+        />
+        <Card
+          title="Toledo, Spain"
+          subtitle="Also know as The Imperial City."
+          tag="Spain"
+          iconName="fas fa-heart"
+          bgPhoto="https://picsum.photos/740/420/?random"
+        />
+        <Card
+          title="Toledo, Spain"
+          subtitle="Also know as The Imperial City."
+          tag="Spain"
+          iconName="fas fa-ellipsis-v"
+          bgPhoto="https://picsum.photos/740/420/?random"
+        />
+        <Card
+          title="Toledo, Spain"
+          subtitle="Also know as The Imperial City."
+          tag="Spain"
+          iconName="fas fa-ellipsis-v"
+          bgPhoto="https://picsum.photos/740/420/?random"
+        />
+        <Card
+          title="Toledo, Spain"
+          subtitle="Also know as The Imperial City."
+          tag="Spain"
+          iconName="fas fa-ellipsis-v"
+          bgPhoto="https://picsum.photos/740/420/?random"
+        />
+      </main>
     </div>
   );
 }
