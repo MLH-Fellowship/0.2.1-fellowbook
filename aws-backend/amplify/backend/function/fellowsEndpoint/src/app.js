46129f410c34beb106bafa1c9a524fa2d291b8a3
Shubham Jain <shubham@sjain.dev>
2020-06-03 13:18:25 +0100

    Rename lambda; Rename username column

--- aws-backend/amplify/backend/function/fellowsEndpoint/src/app.js
+++ aws-backend/amplify/backend/function/fellowsEndpoint/src/app.js
@@ -23,7 +23,7 @@ if(process.env.ENV && process.env.ENV !== "NONE") {
 }
 
 const userIdPresent = false; // TODO: update in case is required to use that definition
-const partitionKeyName = "gh-username";
+const partitionKeyName = "username";
 const partitionKeyType = "S";
 const sortKeyName = "";
 const sortKeyType = "";
