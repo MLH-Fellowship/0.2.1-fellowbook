00ac69a44904dfa6f41fbab7bdd7f6de78ae6bbc
Kenneth Aladi <kennethmichael11@gmail.com>
2020-06-03 12:07:02 +0100

    Started the project

---
+++ web/src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <img src="" alt="MLH logo" />
+            <h1>FellowBook</h1>
+        </div>
+    )
+}
+
+export default Header;
