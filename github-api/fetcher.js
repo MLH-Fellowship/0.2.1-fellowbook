1387fe2d2b4817b3dca708c69fcb6a79ed337106
Shubham Jain <shubham@sjain.dev>
2020-06-04 10:43:19 +0100

    Add function to upload users to AWS Amplify NoSQL db

--- github-api/fetcher.js
+++ github-api/fetcher.js
@@ -127,20 +127,49 @@ const getUserDetails = async ({ usernames }) => {
 	return json.data;
 };
 
-const saveUsers = users => {
-	// TODO: Upload user data into AWS Amplify instead of dumping it as JSON.
+// If we want to upload from existing file, just don't pass in anything!
+const uploadUsers = async (users) => {
+	if (!users) {
+		const usersFile = fs.readFileSync('./allUsers.json', 'utf8');
+		users = JSON.parse(usersFile);
+	}
 
+	const usersCount = users.length;
+	for (let i = 0; i < usersCount; i++) {
+		const user = users[i];
+		if (i % 10 == 0) console.log(`Saving user ${user.username} (${i + 1}/${usersCount}) to amplify db`);
+
+		const response = await fetch(
+			`https://a5c6y99l3g.execute-api.eu-central-1.amazonaws.com/devv/fellows/${user.username}`,
+			{
+				method: 'put',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(user),
+			},
+		);
+
+		if (response.status !== 200) throw Error(response);
+	};
+
+	console.log(`Saved ${usersCount} to aws db`);
+};
+
+const saveUsers = async users => {
 	// save all users a single big JSON file
 	const allUsersString = prettyStringify(users, { indent: 2 });
 	fs.writeFileSync("./allUsers.json", allUsersString, "utf8");
-	console.log(`Saved ${users.length} users to ./allUsers.json`);
+	console.log(`Saved ${usersCount} users to ./allUsers.json`);
 
 	// save username array, for querying single users for more info that is not provided by /orgs/MLH-Fellowship/
 	let usernames = { data: [] };
 	for (let user of users) usernames.data.push(user.username);
 	const allUsernamesString = prettyStringify(usernames, { indent: 2 });
 	fs.writeFileSync("./usernames.json", allUsernamesString, "utf8");
-	console.log(`Saved ${users.length} users to ./usernames.json`);
+	console.log(`Saved ${usersCount} users to ./usernames.json`);
+
+	await uploadUsers(users);
 };
 
 fetchUsers().then(users => saveUsers(users));
