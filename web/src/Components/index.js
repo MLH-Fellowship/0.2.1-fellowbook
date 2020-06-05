267f32fec3cf1141021241cbbdcb16152f269bf8
Shubham Jain <shubham@sjain.dev>
2020-06-05 09:17:34 +0100

    Use user's access token for fetching data from API

--- web/src/Components/index.js
+++ web/src/Components/index.js
@@ -2,18 +2,18 @@ import axios from "axios";
 
 const apiUrl =
   "https://a5c6y99l3g.execute-api.eu-central-1.amazonaws.com/devv/fellows/list";
-const token = "f7915df19de9abe0580de9aff357e9063c4e6093";
 
-export const fetchData = async () => {
+export const fetchData = async (accessToken) => {
   try {
     const response = await axios.get(apiUrl, {
       headers: {
-        Authorization: token,
+        Authorization: accessToken,
       },
     });
 
     return response.data;
   } catch (error) {
+    return [];
     console.log(error);
   }
 };
