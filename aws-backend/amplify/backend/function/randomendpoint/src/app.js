31d88e88e9206319386905f57e40f1dc9e225fbf
Shubham Jain <shubham@sjain.dev>
2020-06-17 21:20:04 +0100

    Add /random endpoint
    This adds a /random endpoint to get a single random fellow.
    
    You can also pass a ?query=blah parameter to get a list of fellows who's
    pod name partly matches the query, or ID matches the whole query.
    
    e.g. ?query=dodo will match the Distributed Dodos, but only ?query=0.2.1
    will work to get them if using a code.

---
+++ aws-backend/amplify/backend/function/randomendpoint/src/app.js
@@ -0,0 +1,67 @@
+const AWS = require('aws-sdk')
+const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+const bodyParser = require('body-parser')
+const express = require('express')
+
+AWS.config.update({ region: process.env.TABLE_REGION });
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+let tableName = "fellowsdb";
+if (process.env.ENV && process.env.ENV !== "NONE") {
+  tableName = tableName + '-' + process.env.ENV;
+}
+
+const path = "/random";
+
+function filterFellows(fellows, query) {
+  query = query.toLowerCase();
+  // Get all fellows where the query is part of the pod name, or the query is the pod ID
+  return fellows.filter(fellow =>
+    fellow.pod.toLowerCase().includes(query) ||
+    fellow.pod_id === query
+  );
+}
+
+// declare a new express app
+const app = express()
+app.use(bodyParser.json())
+app.use(awsServerlessExpressMiddleware.eventContext())
+
+// Enable CORS for all methods
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+});
+
+app.get(path, function (req, res) {
+  dynamodb.scan({
+    TableName: tableName,
+  }, (err, data) => {
+    if (err) {
+      res.statusCode = 500;
+      res.json({ error: 'Could not load items: ' + err.message });
+    } else {
+      const fellows = data.Items;
+      if (req.query.query) {
+        // If there's a query, we want to get all fellows matching the query
+        const filteredFellows = filterFellows(fellows, req.query.query);
+        res.json(filteredFellows);
+      } else {
+        // Otherwise we just want a single random fellow
+        const randomFellow = fellows[Math.floor(Math.random() * fellows.length)];
+        res.json(randomFellow);
+      }
+    }
+  });
+});
+
+app.listen(3000, function () {
+  console.log("App started")
+});
+
+// Export the app object. When executing the application local this does nothing. However,
+// to port it to AWS Lambda we will create a wrapper around that will load the app from
+// this file
+module.exports = app
