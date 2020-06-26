c636014dabfe4eb3c33d67ff599489fece914c99
Shubham Jain <shubham@sjain.dev>
2020-06-26 23:55:18 +0100

    Use GraphQL API entirely to get the GitHub data
    It's easier to understand and maintain if it's just one query. The
    GraphQL API is also really easy to add/remove fields from in future if
    needed.

--- github-api/fetcher.js
+++ github-api/fetcher.js
@@ -1,199 +1,155 @@
 const fetch = require("node-fetch");
 const fs = require("fs");
-const dotenv = require("dotenv");
-const prettyStringify = require("json-stringify-pretty-compact");
+require("dotenv").config();
 
-dotenv.config();
+const query = `
+{
+  organization(login: "MLH-Fellowship") {
+    teams(first: 50) {
+      edges {
+        node {
+          description
+          name
+          id
+          members {
+            nodes {
+              avatarUrl
+              bio
+              email
+              followers {
+                totalCount
+              }
+              following {
+                totalCount
+              }
+              location
+              login
+              name
+              twitterUsername
+              url
+              websiteUrl
+              company
+            }
+          }
+        }
+      }
+    }
+  }
+}`;
 
-const getPodName = team => {
-	// 'name' is ID; 'description' is name
-	if (team.name.startsWith('Pod')) {
-		return team.description === '' ? team.name : team.description;
-	} else {
-		return team.name;
-	}
-};
-
-const escapeUsername = username => username.replace(/[-_]/g, '');
-
-const getTeams = async () => {
-	// <100 teams so first page will do!
-	const response = await fetch(
-		`https://api.github.com/orgs/MLH-Fellowship/teams`,
-		{ headers: { authorization: "token " + process.env.GITHUB_TOKEN } }
-	);
-	return response.json();
-};
-
-const getMembersForTeam = async ({ teamSlug }) => {
-	// <100 members per team so first page will do!
-	const response = await fetch(
-		`https://api.github.com/orgs/MLH-Fellowship/teams/${teamSlug}/members`,
-		{ headers: { authorization: "token " + process.env.GITHUB_TOKEN } }
-	);
-	return response.json();
+const getPodName = (name, description) => {
+  // 'name' is ID; 'description' is name
+  if (name.startsWith('Pod')) {
+    return description === '' ? name : description;
+  } else {
+    return name;
+  }
 };
 
 const fetchUsers = async () => {
-	console.log("Retrieving MLH Fellowship users from GitGub API");
+  const response = await fetch(
+    'https://api.github.com/graphql',
+    {
+      method: 'post',
+      headers: {
+        Authorization: "bearer " + process.env.GITHUB_TOKEN,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ query })
+    },
+  );
+  const json = await response.json();
+  const teams = json.data.organization.teams.edges;
 
-	const teams = await getTeams();
-	const teamsCount = teams.length;
-	console.log(`Fetched ${teamsCount} teams`);
+  const users = [];
+  teams.forEach(obj => {
+    team = obj.node;
 
-	const users = [];
-	const extraUserFields = ['name', 'bio', 'company', 'email', 'location', 'twitterUsername', 'websiteUrl'];
+    // Skip the parent teams; CTF would include people in other pods, so ignore that too
+    if (['TTP Fellows (Summer 2020)', 'CTF', 'MLH Fellows (Summer 2020)'].includes(team.name)) return;
 
-	for (let i = 0; i < teamsCount; i++) {
-		const team = teams[i];
+    const members = team.members.nodes;
+    members.forEach(user => {
+      users.push({
+        avatar_url: user.avatarUrl,
+        bio: user.bio,
+        email: user.email,
+        followers: user.followers.totalCount,
+        following: user.following.totalCount,
+        location: user.location,
+        username: user.login.toLowerCase(),
+        username_original: user.login,
+        name: user.name,
+        twitter_username: user.twitterUsername,
+        github_url: user.url,
+        website_url: user.websiteUrl,
+        company: user.company,
+        pod: getPodName(team.name, team.description),
+        pod_id: team.name.replace('Pod ', ''),
+      });
+    });
+  });
 
-		// Skip the parent Summer 2020 team
-		if (team.name === 'Summer 2020') continue;
-
-		const membersForTeam = await getMembersForTeam({ teamSlug: team.slug });
-		const teamMembersCount = membersForTeam.length;
-		console.log(`Fetched ${teamMembersCount} members for team ${team.name}`);
-
-		// Get all the extra data about all the users in this team, e.g. name, bio, etc.
-		const extraUserDetails = await getUserDetails({
-			usernames: membersForTeam.map(m => m.login)
-		});
-
-		for (let j = 0; j < teamMembersCount; j++) {
-			const member = membersForTeam[j];
-			member.pod = getPodName(team);
-			member.pod_id = team.name.replace('Pod ', '');
-
-			// Keys must be alphanumeric only in GraphQL Query
-			const escapedUsername = escapeUsername(member.login);
-
-			// Add the extra data for each member
-			const thisUserDetails = extraUserDetails[escapedUsername];
-			extraUserFields.forEach(field => member[field] = thisUserDetails[field]);
-
-			// Handle the extra nested fields
-			member['followers'] = typeof thisUserDetails.followers.totalCount === 'undefined' ?
-				thisUserDetails.followers : thisUserDetails.followers.totalCount;
-
-			member['following'] = typeof thisUserDetails.following.totalCount === 'undefined' ?
-				thisUserDetails.following : thisUserDetails.following.totalCount;
-
-			member['repositories'] = typeof thisUserDetails.repositories.totalCount === 'undefined' ?
-				thisUserDetails.repositories : thisUserDetails.repositories.totalCount;
-
-			// Rename 'login' to 'username' (primary key of aws amplify db)
-			member.username = member.login;
-			delete member.login;
-
-			// Add a username_original field storing original-cased username
-			// And convert primary key username to lower case
-			// Note: not sure if username_original is important at all, but keeping it for now
-			member.username_original = member.username;
-			member.username = member.username.toLowerCase();
-		}
-
-		users.push(...membersForTeam);
-	}
-
-	return new Promise((resolve, reject) => resolve(users));
-};
-
-const getUserDetails = async ({ usernames }) => {
-	let queryString = '';
-
-	// Delete underscores/hyphens, keys must be alphanumeric only in GraphQL Query
-	usernames.forEach(u => {
-		const escapedUsername = escapeUsername(u);
-		queryString += `${escapedUsername}: user(login: "${u}") { ...UserFragment }\n`
-	});
-
-	const query = `
-		query {
-			${queryString}
-		}
-
-		fragment UserFragment on User {
-			name
-			login
-			bio
-			company
-			email
-			location
-			twitterUsername
-			websiteUrl
-			followers {
-				totalCount
-			}
-			following {
-				totalCount
-			}
-			repositories {
-				totalCount
-			}
-		}`;
-
-	const response = await fetch(
-		'https://api.github.com/graphql',
-		{
-			method: 'post',
-			headers: {
-				Authorization: "bearer " + process.env.GITHUB_TOKEN,
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({ query })
-		},
-	);
-
-	const json = await response.json();
-	return json.data;
-};
+  return users;
+}
 
 // If we want to upload from existing file, just don't pass in anything!
 const uploadUsers = async (users) => {
-	if (!users) {
-		const usersFile = fs.readFileSync('./allUsers.json', 'utf8');
-		users = JSON.parse(usersFile);
-	}
+  if (!users) {
+    const usersFile = fs.readFileSync('./allUsers.json', 'utf8');
+    users = JSON.parse(usersFile);
+  }
 
-	const usersCount = users.length;
-	for (let i = 0; i < usersCount; i++) {
-		const user = users[i];
-		if (i % 10 == 0) console.log(`Saving user ${user.username} (${i + 1}/${usersCount}) to amplify db`);
+  const usersCount = users.length;
 
-		const response = await fetch(
-			`https://ld48eii9kk.execute-api.eu-central-1.amazonaws.com/dev/fellows/${user.username}`,
-			{
-				method: 'put',
-				headers: {
-					'Content-Type': 'application/json',
-					'Authorization': process.env.GITHUB_TOKEN
-				},
-				body: JSON.stringify(user),
-			},
-		);
+  // Batch of 20s, avoid API timeouts
+  for (let i = 0; i < usersCount; i += 20) {
+    const response = await fetch(
+      // NOTE: this is a bit hacky, but we want to use the PUT request to insert ALL the users.
+      // This saves us from calling the API authorizer function for EVERY fellow, which might lead to
+      // GitHub API rate limiting for lots of consecutive calls
+      // For this endpoint, we're going to ignore the username in the URL, and just use the username
+      // from the body.
+      `https://ld48eii9kk.execute-api.eu-central-1.amazonaws.com/dev/fellows/randomfellowthatdoesnotexist`,
+      {
+        method: 'put',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': process.env.GITHUB_TOKEN
+        },
+        body: JSON.stringify(users.slice(i, i + 20)),
+      },
+    );
 
-		if (response.status !== 200) throw Error(response);
-	};
-
-	console.log(`Saved ${usersCount} to aws db`);
+    const json = await response.json();
+    console.error('response', response, json);
+  }
 };
 
 const saveUsers = async users => {
-	const usersCount = users.length;
+  const usersCount = users.length;
 
-	// save all users a single big JSON file
-	const allUsersString = prettyStringify(users, { indent: 2 });
-	fs.writeFileSync("./allUsers.json", allUsersString, "utf8");
-	console.log(`Saved ${usersCount} users to ./allUsers.json`);
+  // save all users as a single big JSON file
+  const allUsersString = JSON.stringify(users, null, 2);
+  fs.writeFileSync("./allUsers.json", allUsersString, "utf8");
+  console.log(`Saved ${usersCount} users to ./allUsers.json`);
 
-	// save username array, for querying single users for more info that is not provided by /orgs/MLH-Fellowship/
-	let usernames = { data: [] };
-	for (let user of users) usernames.data.push(user.username);
-	const allUsernamesString = prettyStringify(usernames, { indent: 2 });
-	fs.writeFileSync("./usernames.json", allUsernamesString, "utf8");
-	console.log(`Saved ${usersCount} users to ./usernames.json`);
+  // save username array, for querying single users for more info that is not provided by /orgs/MLH-Fellowship/
+  let usernames = { data: [] };
+  for (let user of users) usernames.data.push(user.username);
+  const allUsernamesString = JSON.stringify(usernames, null, 2);
+  fs.writeFileSync("./usernames.json", allUsernamesString, "utf8");
+  console.log(`Saved ${usersCount} users to ./usernames.json`);
 
-	await uploadUsers(users);
+  return users;
 };
 
-fetchUsers().then(users => saveUsers(users));
+const useSaved = true;
+if (useSaved) {
+  uploadUsers();
+} else {
+  fetchUsers()
+    .then(users => saveUsers(users))
+    .then(users => uploadUsers(users))
+    ;
+}
